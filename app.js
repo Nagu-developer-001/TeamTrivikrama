92a5273876001d96c9af3c2479e42f07b7b55c3a
Nagu-developer-001 <nagabhoshanabhat@gmail.com>
2024-10-23 16:13:39 +0530

    UPDATED WITH STYLE

--- app.js
+++ app.js
@@ -23,13 +23,23 @@ main().then((res)=>{
 async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/wonderLust");
 }
-
+function wrapAsync(fn){
+    return function(req,res,next){
+        fn(req,res,next).catch((err)=>{
+            next(err);
+        });
+    }
+}
+app.use("/api",(req,res,next)=>{
+    console.log("This is middleware !!! for/here we do athentication");
+    next();
+});
 //TODO CREATE ROUTE
 app.get("/",(req,res)=>{
     res.send("GET is working!!!... .. .");
 });
 //todo TESTING ROUTE
-app.get("/TestListings",async(req,res)=>{
+app.get("/api/TestListings",async(req,res)=>{
     place = [{
                 title: "Cozy Beachfront Cottage111",
                 description:
@@ -46,45 +56,61 @@ app.get("/TestListings",async(req,res)=>{
         console.log(x);
 });
 //TODO INDEX ROUTE
-app.get("/Listing",async(req,res)=>{
+app.get("/Listing",wrapAsync(async(req,res)=>{
     let allListing = await placeList.find({});
     console.log(allListing[0].title);
     res.render("listings/index.ejs",{allListing});
-});
+}));
 //TODO NEW ROUTE
-app.get("/Listing/new",(req,res)=>{
+app.get("/Listing/new",wrapAsync((req,res)=>{
     res.render("listings/newForm.ejs");
-});
+}));
 //TODO SHOW ROUTE
-app.get("/Listing/:id",async(req,res)=>{
+app.get("/Listing/:id",wrapAsync(async(req,res)=>{
     let {id} = req.params;
     let content = await placeList.findById(id);
     res.render("listings/show.ejs",{content});
-});
+}));
 //TODO CREATE ROUTE
-app.post("/Listing",async(req,res)=>{
+app.post("/Listing",wrapAsync(async(req,res)=>{
     let palceLists = req.body.Listing;//TODO ANOTHER WAY TO GET THE MULTIPLE DATA FROM FORM ...
     let placeAdd = await placeList.insertMany(palceLists);
+    console.log(placeAdd);
     res.redirect("/listing");
-});
+}));
 //TODO EDIT ROUTE
-app.get("/listing/:id/edits",async(req,res)=>{
+app.get("/listing/:id/edits",wrapAsync(async(req,res)=>{
     let {id} = req.params;
     let content = await placeList.findById(id);
     console.log(content);
     res.render("listings/edit.ejs",{content});
-});
+}));
 //TODO UPDATE ROUTE
-app.put("/listing/:id",async(req,res)=>{
+app.put("/listing/:id",wrapAsync(async(req,res)=>{
     let {id} = req.params;
     let updated = await placeList.findByIdAndUpdate(id,{...req.body.Listing});
     res.redirect("/listing");
-});
+}));
 //TODO DELETE ROUTE
-app.delete("/listing/:id",async(req,res)=>{
+app.delete("/listing/:id",wrapAsync(async(req,res)=>{
     let {id} = req.params;
     let deleteEle = await placeList.findByIdAndDelete(id);
     res.redirect("/listing");
+}));
+function validateErr(err){
+    console.log(err);
+    return err;
+}
+//TODO ERROR HANDILNG ROUTE
+app.use((err,req,res,next)=>{
+    if(err.name == "ValidationError"){
+        err = validateErr(err);
+        next(err);
+    }
+});
+app.use((err,req,res,next)=>{
+    let {status=500,message="server went wrong"} = err;
+    res.status(status).send(message);
 });
 app.listen(port,(req,res)=>{
     console.log(`The server has been Started at localhost // - ${port}`);
